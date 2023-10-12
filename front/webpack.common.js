d0319d0999bea9427c1b438600d3e49cca59e41e
marinaustinovich <115401561+marinaustinovich@users.noreply.github.com>
2023-10-12 12:35:13 +0300

    add settings  for deploy

--- front/webpack.common.js
+++ front/webpack.common.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
-const TerserPlugin = require('terser-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin'); 
+const TerserPlugin = require('terser-webpack-plugin'); 
 
 module.exports = {
   target: 'web',
