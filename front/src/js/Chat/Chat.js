f0584bfb232c3d5dd541991656dda2f8853c9996
marinaustinovich <115401561+marinaustinovich@users.noreply.github.com>
2023-10-13 13:20:32 +0300

    fix errors

--- front/src/js/Chat/Chat.js
+++ front/src/js/Chat/Chat.js
@@ -51,7 +51,7 @@ export default class Chat {
   }
 
   onEnterChatHandler() {
-    this.websocket = new WebSocket('ws://ws-chat-r75q.onrender.com');
+    this.websocket = new WebSocket('wss://ws-chat-r75q.onrender.com');
     this.subscribeOnEvents();
   }
 
